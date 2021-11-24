876a1340b3a83c906ea73940b74d4776f16d1eec
= <eduardo.plis@hotmail.com>
2021-11-24 10:10:44 -0300

    Log

---
+++ src/main/java/br/com/alura/ecommerce/LogService.java
@@ -0,0 +1,56 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer< String, String>(properties());
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()){
+                System.out.println("Encontrei: " + records.count() + " Registros");
+                for(var record: records){
+                    System.out.println("-------------------------------------------");
+                    System.out.println("Log");
+                    System.out.println(record.topic());
+                    System.out.println(record.key());
+                    System.out.println(record.value());
+                    System.out.println(record.partition());
+                    System.out.println(record.offset());
+
+                    try {
+                        Thread.sleep(1000);;
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Email enviado!");
+
+                }
+            }
+
+
+        }
+
+    }
+
+    private static Properties properties() {
+
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+
+        return properties;
+    }
+}
